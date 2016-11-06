596ea7adf8cce3df33b57082f6da8a8aa55468c5
Versatile <its@clyde.pw>
2016-11-06 16:35:23 -0500

    dont use that name either

--- chickenpepperoni/Globals.py
+++ chickenpepperoni/Globals.py
@@ -14,7 +14,7 @@ MaxEnemyAttack = 2000
 MaxEnemyDefense = 1000
 
 # Everything relating to the player
-DisallowedNames = ['shit flavored hot dog man', 'traphouse lord']
+DisallowedNames = ['shit flavored hot dog man', 'traphouse lord', 'brendan dolan']
 PlayerMovements = {
     pygame.K_LEFT: (-1, 0),
     pygame.K_RIGHT: (1, 0),
