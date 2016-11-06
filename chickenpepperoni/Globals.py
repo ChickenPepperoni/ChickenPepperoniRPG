32954edd140e4d6f61026fb8d18049ee6a0d752a
GroverTTC <groverttc@gmail.com>
2016-11-06 16:27:57 -0500

    this ones for you jake

--- chickenpepperoni/Globals.py
+++ chickenpepperoni/Globals.py
@@ -14,6 +14,7 @@ MaxEnemyAttack = 2000
 MaxEnemyDefense = 1000
 
 # Everything relating to the player
+DisallowedNames = ['shit flavored hot dog man', 'traphouse lord']
 PlayerMovements = {
     pygame.K_LEFT: (-1, 0),
     pygame.K_RIGHT: (1, 0),
