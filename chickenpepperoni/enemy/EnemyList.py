9de3c2dd31139a646ad0d9ad774b047eeeceb3a9
GroverTTC <groverttc@gmail.com>
2016-11-06 20:33:31 -0500

    nothing

--- chickenpepperoni/enemy/EnemyList.py
+++ chickenpepperoni/enemy/EnemyList.py
@@ -1,5 +1,11 @@
 from chickenpepperoni.enemy.EnemyPepperoni import EnemyPepperoni
 
+'''
+Define all the enemies here.
+
+ARGS: ('NAME', HP, LEVEL, ATTACK, DEFENSE, EXP, ELITE=TRUE/FALSE)
+'''
+
 # Pepperoni Enemies
 PepperoniMinion = EnemyPepperoni('Pepperoni Minion', 10, 1, 2, 0, 3)
 PepperoniGuard = EnemyPepperoni('Pepperoni Guard', 15, 1, 3, 0, 5, elite=True)
